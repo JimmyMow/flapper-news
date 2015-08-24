3cc52f7b119e21c677026fed1ac00bae60be0d08
Jack Mallers <jimmymowschess@gmail.com>
2015-08-24 16:20:30 -0500

    first commit

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,49 @@
+angular.module('flapperNews', ['ui.router', 'templates', 'Devise'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      resolve: {
+        postPromise: ['posts', function(posts){
+          return posts.getAll();
+        }]
+      },
+      url: '/home',
+      templateUrl: 'home/_home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+      resolve: {
+        post: ['$stateParams', 'posts', function($stateParams, posts) {
+          return posts.get($stateParams.id);
+        }]
+      },
+      url: '/posts/{id}',
+      templateUrl: 'posts/_posts.html',
+      controller: 'PostsCtrl'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'auth/_login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'auth/_register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  before_filter :authenticate_user!, only: [:create, :upvote]
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = post.comments.create(comment_params)
+    render json: comment
+  end
+
+  def upvote
+    post = Post.find(params[:post_id])
+    comment = post.comments.find(params[:id])
+    comment.increment!(:upvotes)
+
+    render json: comment
+  end
+private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
